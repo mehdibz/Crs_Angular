ad847d1be8cf54f1cadc0f2e4da0d085793fddc5
Mehdi <bz.mehdi@gmail.com>
2017-07-22 15:04:11 -0700

    Assignment_1

--- App.js
+++ App.js
@@ -2,17 +2,19 @@
 'use strict';
 
 angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
+
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
 
   $scope.menu="";
+  $scope.Num=0;
   $scope.TestMenu= function () {
     calculate($scope.menu);
   };
 
   function calculate(string) {
     var count=-1,parts="";
-    
+
     while (string.match(",,")) {
       string = string.replace(/,,/i, ',');
       string = string.replace(/ /i, ',');
@@ -25,13 +27,16 @@ function LunchCheckController($scope) {
     while(angular.equals(string, "")&(count==1)){
       $scope.result= "Please enter data first";
       count=0;
+      $scope.Num= count;
     }
     while(count>3){
       $scope.result= "Too much!";
+      $scope.Num= count;
       count=0;
     }
     while(count>0){
       $scope.result= "Enjoy!";
+      $scope.Num= count;
       count=0;
     }
   };
